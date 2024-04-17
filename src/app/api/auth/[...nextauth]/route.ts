6f749f947252d96859bce264e5fa8f74f4f32313
rayruenheck <rayruenheck@gmail.com>
2024-04-17 19:33:24 -0400

    fixed sign in with google so we can deploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,2 @@
-// imports
-import NextAuth from "next-auth"
-
-// importing providers
-
-import GoogleProvider from "next-auth/providers/google";
-
-const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-          })
-    ],
-    callbacks: {
-      jwt: async ({ token, user }) => {
-        return { ...token, ...user };
-      },
-      session: async ({ session, token }) => {
-        session.user = token;
-        return session;
-      },
-    },
-    secret: process.env.NEXTAUTH_SECRET
-})
-
-export { handler as GET, handler as POST }
+import { handlers } from "@/auth" // Referring to the auth.ts we just created
+export const { GET, POST } = handlers
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,6 @@ export default function Home() {
     <div className="w-full h-screen flex flex-col justify-center items-center">
         <p className="text-2xl mb-2">Not Signed In</p>
         <button className="bg-blue-600 py-2 px-6 rounded-md mb-2" onClick={() => signIn('google')}>Sign in with google</button>
-        <button className="bg-none border-gray-300 border py-2 px-6 rounded-md mb-2" onClick={() => signIn('github')}>Sign in with github</button>
     </div>
   )
 
