f2e9471eb4bf06d65cbada23170c7b9357236016
Ray Ruenheck <rayruenheck@gmail.com>
2024-05-08 15:13:50 -0400

    more work to do to make sure check boxes are checked as one

--- src/app/components/taskcard.tsx
+++ src/app/components/taskcard.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Task } from './interfaces';
 
 type TaskCardProps = {
     task: Task;  // Define the type for props, indicating it expects an object with a 'task' property
 };
 
-
 const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
-    
+    const [completed, setCompleted] = useState(task.isCompleted);
+
     const handleCheckboxChange = (id: string, isCompleted: boolean) => {
+        const newCompletedStatus = !isCompleted;
         fetch('http://localhost:5000/update-completion', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ id, isCompleted: !isCompleted }) 
+            body: JSON.stringify({ id, isCompleted: newCompletedStatus })
         })
         .then(response => response.json())
         .then(data => {
             if (data.status === "success") {
                 console.log('Update successful:', data.message);
-                
+                setCompleted(newCompletedStatus);
             } else {
                 console.error('Failed to update task:', data.message);
             }
@@ -35,11 +36,11 @@ const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
             <div className="flex items-center">
                 <input
                     type="checkbox"
-                    checked={task.isCompleted}
-                    onChange={() => handleCheckboxChange(task.id, task.isCompleted)}
+                    checked={completed}
+                    onChange={() => handleCheckboxChange(task.id, completed)}
                     className="form-checkbox h-5 w-5 text-blue-600 mr-2"
                 />
-                <span className={`${task.isCompleted ? 'line-through' : ''} flex-grow`}>{task.name}</span>
+                <span className={`${completed ? 'line-through' : ''} flex-grow`}>{task.name}</span>
             </div>
             <div className="text-sm text-gray-700 mt-2">
                 <p>Priority: {task.priority}</p>
@@ -49,4 +50,4 @@ const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
     );
 };
 
-export default TaskCard
+export default TaskCard;
